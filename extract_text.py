975c513f1589a586fa4836f86ac44518b3ffe10c
Drumilshah <drumilshah9121997@gmail.com>
2022-07-07 22:22:52 +0530

    extract text from pdf and docx files and store it

---
+++ extract_text.py
@@ -0,0 +1,34 @@
+import os
+from PyPDF2 import PdfFileReader
+import textract
+
+
+# extract text from pdfs and docs for annotation
+
+datasetPath = "/Users/drumilshah/Documents/ResumeParser/dataset"
+
+trainingSetCount = 18
+count = 0
+trainingSetPdfs = []
+text = ""
+for root, dirs, files in os.walk(datasetPath):
+  for filename in files:
+    count += 1
+    trainingSetPdfs.append(filename)
+    name, extension = os.path.splitext(filename)
+    filePath = os.path.join(datasetPath, filename)
+    if(extension == ".pdf"):
+      pdfFileObj = open(filePath, "rb")
+      pdfReader = PdfFileReader(pdfFileObj)
+      pages = pdfReader.numPages
+      for page in range(pages):
+        pdfObj = pdfReader.getPage(page)
+        text = pdfObj.extractText()
+        with open(f'./extracted_text/{name}.txt', 'w') as f:
+          f.write(text)
+    elif(extension == ".doc" or extension == ".docx"):
+      text = str(textract.process(filePath))
+      with open(f'./extracted_text/{name}.txt', 'w') as f:
+          f.write(text)
+    if(count > trainingSetCount):
+      break
